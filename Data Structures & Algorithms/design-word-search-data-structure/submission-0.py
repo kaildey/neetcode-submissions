89e47366365398b9e9fa1c2ef449ba45017a23be
kaildey <137431691+kaildey@users.noreply.github.com>
2026-08-09 20:37:30 +0500

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        
+        curr.end = True
+
+    def helper(self, root: TrieNode, word: str):
+        if not word:
+            return root.end
+
+        for char in root.children:
+            if word[0] != '.' and word[0] != char:
+                continue
+            if self.helper(root.children[char], word[1:]):
+                return True
+
+        return False            
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        
+        for i in range(len(word)):
+            if word[i] == '.':
+                return self.helper(curr, word[i:])
+            
+            if word[i] not in curr.children:
+                return False
+
+            curr = curr.children[word[i]]
+        
+        return curr.end
